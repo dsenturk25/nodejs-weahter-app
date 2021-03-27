c384f9f57395444d2a199c1dab01d84495ecf4de
Doğu Boran Şentürk <dsenturk25@my.uaa.k12.tr>
2021-03-27 22:53:10 +0300

    few changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longtitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=8fd8d422eecb6841248d8f14878cad52&query='+ encodeURIComponent(latitude) + ', ' + encodeURIComponent(longtitude) + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=8fd8d422eecb6841248d8f14878cad52&query='+ encodeURIComponent(latitude) + ', ' + encodeURIComponent(longtitude) + '&units=m'
 
     request({ url, json: true }, (error, {body}) => {
         if (error) {
@@ -10,7 +10,7 @@ const forecast = (latitude, longtitude, callback) => {
             callback('Unable to find the location', undefined)
         }else {
             const current = body.current
-            callback(undefined, `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feelslike ${current.feelslike} degrees out.`)
+            callback(undefined, `${current.weather_descriptions[0]}. It is currently ${current.temperature} degrees out. It feelslike ${current.feelslike} degrees out. Humidity level is ${current.humidity}`)
         }    
     })
 }
